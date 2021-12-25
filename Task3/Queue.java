5d73a724999f5beebefb5ff869ac89a87b9a0083
mikomissa <mikomissa@gmail.com>
2021-12-25 21:07:08 +0200

    Task3 some files

--- Task3/Queue.java
+++ Task3/Queue.java
@@ -34,6 +34,7 @@ public class Queue {
     }
 
     public void push(Integer value) {
+        // додавання елемента
         Node newTail = new Node(value, new AtomicReference<>(null));
         while (true) {
             Node tail = tailRef.get();
@@ -47,6 +48,7 @@ public class Queue {
     }
 
     public Integer pop() throws NoSuchElementException {
+        // видалення елемента
         while (true) {
             Node head = headRef.get();
             Node tail = tailRef.get();
