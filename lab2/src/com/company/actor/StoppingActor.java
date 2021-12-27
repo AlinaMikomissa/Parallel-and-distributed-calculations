b60e10af340e1ca3a08f67a2d1cea9bd27701c8e
mikomissa <mikomissa@gmail.com>
2021-12-27 22:00:59 +0200

    lab 2 - Trams and Stoppings

---
+++ lab2/src/com/company/actor/StoppingActor.java
@@ -0,0 +1,56 @@
+package com.company.actor;
+
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class StoppingActor extends AbstractActor {
+
+    private static final Integer MAX = 2;
+    private final List<ActorRef> stayed = new ArrayList<>();
+    private Integer allocated = 0;
+    private String name;
+
+    public StoppingActor(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals(TramActor.Command.CAN_I_STOP, command -> checkIfAvailable())
+                .matchEquals(TramActor.Command.STOP_ON_STOPPING, command -> regisNewTram())
+                .matchEquals(TramActor.Command.LEAVING, command -> registerLeave())
+                .build();
+    }
+
+    private void registerLeave() {
+        allocated--;
+        stayed.remove(sender());
+    }
+
+    private void regisNewTram() {
+        stayed.add(sender());
+    }
+
+    private void checkIfAvailable() {
+        if (allocated < MAX) {
+            allocated++;
+            sender().tell(Command.STOPPING_IS_EMPTY, self());
+            if(Objects.equals(allocated, MAX)) {
+                System.out.println("Трамвай "+ name + " повний");
+            }
+        } else {
+            sender().tell(Command.STOPPING_IS_FULL, self());
+        }
+    }
+
+    public enum Command {
+        STOPPING_IS_EMPTY,
+        STOPPING_IS_FULL
+    }
+}
---
+++ lab2/src/com/company/actor/TramActor.java
@@ -0,0 +1,63 @@
+package com.company.actor;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+import java.util.List;
+
+public class TramActor extends AbstractActor {
+
+
+    private final List<ActorRef> stoppings;
+    private ActorRef nearest;
+    private final String name;
+
+    public TramActor(List<ActorRef> stoppings, String name) {
+        this.stoppings = stoppings;
+        this.name = name;
+        tryStopOnStopping();
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals(StoppingActor.Command.STOPPING_IS_EMPTY, command -> stopOnStopping())
+                .matchEquals(StoppingActor.Command.STOPPING_IS_EMPTY, command -> tryStopOnStopping())
+                .build();
+    }
+
+    private void tryStopOnStopping() {
+        nearest = getNearestStation();
+        nearest.tell(Command.CAN_I_STOP, self());
+    }
+
+    private ActorRef getNearestStation() {
+        return stoppings.get((int) (Math.random() * stoppings.size()));
+    }
+
+    private void stopOnStopping() {
+        nearest.tell(Command.STOP_ON_STOPPING, self());
+        System.out.println("Трамвай "+ name + " зупинився");
+        try {
+            Thread.sleep(5000);
+            if (Math.random() > 0.7) {
+                leave();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void leave() {
+        nearest.tell(Command.LEAVING, ActorRef.noSender());
+        System.out.println("Трамвай "+ name + " покидає зупинку");
+    }
+
+    public enum Command {
+        STOP_ON_STOPPING,
+        CAN_I_STOP,
+        LEAVING
+    }
+}
+
