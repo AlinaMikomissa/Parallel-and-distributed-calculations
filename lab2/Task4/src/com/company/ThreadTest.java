313bfc303995fdf470ebc5af4845692aac10a4e9
mikomissa <mikomissa@gmail.com>
2021-12-28 08:08:08 +0200

    Very many adjustments of Tram and Stoppings Actors + folders system optimization
    Very many adjustments of Tram and Stoppings Actors + folders system optimization

---
+++ lab2/Task4/src/com/company/ThreadTest.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class ThreadTest implements Runnable {
+
+    private HarrisList<String> harrisList;
+
+    public ThreadTest(HarrisList<String> harrisList) {
+        this.harrisList = harrisList;
+    }
+
+    @Override
+    public void run() {
+        String currThreadName = Thread.currentThread().getName();
+        harrisList.push(currThreadName);
+
+        if (currThreadName.equals("Thread-4")) {
+            System.out.println("Remove: " + harrisList.pop(currThreadName));
+        }
+    }
+}
---
+++ lab2/src/main/java/Main.java
@@ -0,0 +1,35 @@
+
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import actor.TramActor;
+import actor.StoppingActor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("TRAM-STOPPING-PROBLEM");
+        List<ActorRef> stoppings = IntStream.range(1, 4)
+                .boxed()
+                .map(i -> system.actorOf(Props.create(StoppingActor.class, "№ - " + i)))
+                .collect(Collectors.toList());
+
+        List<ActorRef> trams = IntStream.range(1, 11)
+                .boxed()
+                .map(i -> system.actorOf(Props.create(TramActor.class, stoppings, "Трамвай - " + i)))
+                .collect(Collectors.toList());
+
+        try {
+            Thread.sleep(30000);
+            trams.forEach(system::stop);
+            stoppings.forEach(system::stop);
+            system.terminate();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
